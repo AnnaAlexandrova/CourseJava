d9f361c345d6539bcc3b4d4ed321c29e1830b050
AnnaAlexandrova <alexandrova-anna@bk.ru>
2019-05-19 13:25:38 +0700

    Задача Tree исправления

--- Tree/src/fytyr/idea_projects/course_java/tree/Tree.java
+++ Tree/src/fytyr/idea_projects/course_java/tree/Tree.java
@@ -35,22 +35,25 @@ public class Tree<T> {
     private int compare(T data1, T data2) {
         if (comparator != null) {
             return comparator.compare(data1, data2);
-        } else {
-            //noinspection unchecked
-            Comparable<T> c = (Comparable<T>) data1;
-            return c.compareTo(data2);
         }
+        if (data1 == null || data2 == null) {
+            return (data1 == null) ? -1 : 1;
+        }
+        //noinspection unchecked
+        Comparable<T> c = (Comparable<T>) data1;
+        return c.compareTo(data2);
     }
 
     public boolean contains(T data) {
         TreeNode<T> currentRoot = root;
+
         while (currentRoot != null) {
             int compare = compare(currentRoot.getData(), data);
+
             if (compare == 0) {
                 return true;
             }
-            currentRoot = compare > 0
-                    ? currentRoot.getLeftChild() : currentRoot.getRightChild();
+            currentRoot = compare > 0 ? currentRoot.getLeftChild() : currentRoot.getRightChild();
         }
         return false;
     }
@@ -58,12 +61,14 @@ public class Tree<T> {
     public void add(T data) {
         TreeNode<T> currentRoot = root;
         TreeNode<T> currentParent = null;
+
         while (currentRoot != null) {
             currentParent = currentRoot;
-            currentRoot = compare(currentRoot.getData(), data) > 0
-                    ? currentRoot.getLeftChild() : currentRoot.getRightChild();
+            currentRoot = compare(currentRoot.getData(), data) > 0 ? currentRoot.getLeftChild() : currentRoot.getRightChild();
         }
+
         currentRoot = new TreeNode<>(data);
+
         if (currentParent != null) {
             if (compare(currentParent.getData(), data) > 0) {
                 currentParent.setLeftChild(currentRoot);
@@ -77,37 +82,46 @@ public class Tree<T> {
     }
 
     public boolean remove(T data) {
+        if (root == null) {
+            return false;
+        }
         TreeNode<T> currentRoot = root;
         TreeNode<T> currentParent = null;
-        while (currentRoot != null) {
-            if (compare(currentRoot.getData(), data) == 0) {
-                if (currentRoot.getRightChild() == null && currentRoot.getLeftChild() == null) {
-                    removeIfNoChildren(currentRoot, currentParent);
 
-                } else if (currentRoot.getRightChild() == null && currentRoot.getLeftChild() != null) {
-                    removeIfOneChild(currentRoot, currentParent, currentRoot.getLeftChild());
+        int compareResult = compare(currentRoot.getData(), data);
 
-                } else if (currentRoot.getRightChild() != null && currentRoot.getLeftChild() == null) {
-                    removeIfOneChild(currentRoot, currentParent, currentRoot.getRightChild());
-
-                } else if (currentRoot.getRightChild() != null && currentRoot.getLeftChild() != null) {
-                    removeIfTwoChildren(currentRoot, currentParent);
-                }
-                size--;
-                return true;
+        while (compareResult != 0) {
+            currentRoot = compareResult > 0 ? currentRoot.getLeftChild() : currentRoot.getRightChild();
+            if (currentRoot == null) {
+                return false;
             }
             currentParent = currentRoot;
-            currentRoot = compare(currentRoot.getData(), data) > 0
-                    ? currentRoot.getLeftChild() : currentRoot.getRightChild();
+            compareResult = compare(currentRoot.getData(), data);
         }
-        return false;
+
+        if (currentRoot.getRightChild() == null && currentRoot.getLeftChild() == null) {
+            removeIfNoChildren(currentRoot, currentParent);
+        } else if (currentRoot.getRightChild() == null && currentRoot.getLeftChild() != null) {
+            removeIfOneChild(currentRoot, currentParent, currentRoot.getLeftChild());
+        } else if (currentRoot.getRightChild() != null && currentRoot.getLeftChild() == null) {
+            removeIfOneChild(currentRoot, currentParent, currentRoot.getRightChild());
+        } else {
+            removeIfTwoChildren(currentRoot, currentParent);
+        }
+
+        size--;
+        return true;
     }
 
     private void removeIfNoChildren(TreeNode<T> nodeToRemove, TreeNode<T> parent) {
-        if (parent.getLeftChild() == nodeToRemove) {
-            parent.setLeftChild(null);
+        if (parent != null) {
+            if (parent.getLeftChild() == nodeToRemove) {
+                parent.setLeftChild(null);
+            } else {
+                parent.setRightChild(null);
+            }
         } else {
-            parent.setRightChild(null);
+            this.root = null;
         }
     }
 
@@ -126,11 +140,14 @@ public class Tree<T> {
     private void removeIfTwoChildren(TreeNode<T> nodeToRemove, TreeNode<T> parent) {
         TreeNode<T> minLeft = nodeToRemove.getRightChild().getLeftChild();
         TreeNode<T> minLeftParent = nodeToRemove.getRightChild();
+
         while (minLeft.getLeftChild() != null) {
             minLeftParent = minLeft;
             minLeft = minLeft.getLeftChild();
         }
+
         minLeftParent.setLeftChild(minLeft.getRightChild());
+
         if (parent != null) {
             if (compare(parent.getData(), minLeft.getData()) > 0) {
                 parent.setLeftChild(minLeft);
@@ -140,17 +157,22 @@ public class Tree<T> {
         } else {
             this.root = minLeft;
         }
+
         minLeft.setLeftChild(nodeToRemove.getLeftChild());
         minLeft.setRightChild(nodeToRemove.getRightChild());
     }
 
     public void traverseInWidth(Consumer<T> consumer) {
-        makeTreeIsNotEmptyExceptionCheck();
+        if (root == null) {
+            return;
+        }
         Queue<TreeNode<T>> queue = new LinkedList<>();
         queue.add(root);
+
         while (!queue.isEmpty()) {
             TreeNode<T> node = queue.remove();
             consumer.accept(node.getData());
+
             if (node.getLeftChild() != null) {
                 queue.add(node.getLeftChild());
             }
@@ -161,37 +183,37 @@ public class Tree<T> {
     }
 
     public void traverseInDepthRecursion(Consumer<T> consumer) {
-        makeTreeIsNotEmptyExceptionCheck();
+        if (root == null) {
+            return;
+        }
         visit(root, consumer);
     }
 
     private void visit(TreeNode<T> node, Consumer<T> consumer) {
         if (node != null) {
-            visit(node.getLeftChild(), consumer);
             consumer.accept(node.getData());
+            visit(node.getLeftChild(), consumer);
             visit(node.getRightChild(), consumer);
         }
     }
 
     public void traverseInDepth(Consumer<T> consumer) {
-        makeTreeIsNotEmptyExceptionCheck();
+        if (root == null) {
+            return;
+        }
         Deque<TreeNode<T>> deque = new LinkedList<>();
         deque.addLast(root);
+
         while (!deque.isEmpty()) {
             TreeNode<T> node = deque.removeLast();
             consumer.accept(node.getData());
+
             if (node.getRightChild() != null) {
                 deque.addLast(node.getRightChild());
             }
             if (node.getLeftChild() != null) {
                 deque.addLast(node.getLeftChild());
             }
-        }
-    }
-
-    private void makeTreeIsNotEmptyExceptionCheck() {
-        if (size == 0) {
-            throw new NullPointerException("Дерево не содержит ни одного элемента");
         }
     }
 }
